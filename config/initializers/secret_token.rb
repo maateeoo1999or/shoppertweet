982b5f67e86183f4b72de1c10e2831a7441ca488
Matthew Orth <maateeoo1999or@gmail.com>
2012-03-23 16:23:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoppertweet::Application.config.secret_token = '959b507312a39765dd79ccc06ab9ca37503f5d4ea55cf0d780d2aab44b1d7a73e9b3ec1f7abc29608d809303ba9d51a5c49a8cb8106c48fe15afe6d4747f5386'
